88fcd08ffbf0ea0b55c01874d129aaca6f4643b6
Enrique Naudon <enaudoncs@gmail.com>
2018-05-03 21:57:56 -0400

    Added more comments to command.mli

--- utils/lib/command.mli
+++ utils/lib/command.mli
@@ -3,6 +3,8 @@ type ('tp, 'tm) t = private
   | Bind_term of Identifier.t * 'tm
   | Eval_term of 'tm
 
+(** [bind_term id tm] constructs a command to bind [id] to [tm]. *)
 val bind_term : string -> 'tm -> ('tp, 'tm) t
 
+(** [eval_term tm] constructs a command to evaluate [tm]. *)
 val eval_term : 'tm -> ('tp, 'tm) t
