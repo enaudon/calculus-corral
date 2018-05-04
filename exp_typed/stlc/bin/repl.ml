b7c4a6c20e915f0c04d81f0d78673aa8719f25bc
Enrique Naudon <enaudoncs@gmail.com>
2018-05-03 21:40:23 -0400

    Added val env and deep-reduction flag to Stlc.Repl

--- exp_typed/stlc/bin/repl.ml
+++ exp_typed/stlc/bin/repl.ml
@@ -8,37 +8,46 @@ type mode =
 
 let mode = ref Repl
 
+let deep = ref None
+
 let parse_cmd_args () =
-  let usage_msg = Printf.sprintf "Usage: %s [file]" Sys.argv.(0) in
+  let specs = [
+    ("--deep-beta-reduction", Arg.Unit (fun () -> deep := Some ()),
+      "Beta-reduce within the body of abstractions.") ;
+  ] in
   let parse_file f = match !mode with
     | Repl -> mode := File [f]
     | File fs -> mode := File (f :: fs)
   in
-  Arg.parse [] parse_file usage_msg
+  let usage_msg = Printf.sprintf "Usage: %s [file]" Sys.argv.(0) in
+  Arg.parse specs parse_file usage_msg
 
-let evaluate env lexbuf =
+let evaluate tp_env vl_env lexbuf =
 
-  let evaluate_command env cmd = match cmd with
+  let deep = !deep in
+
+  let evaluate_command (tp_env, vl_env) cmd = match cmd with
     | Command.Bind_term (id, tm) ->
-      let tp = Term.to_type ~env tm in
+      let tp = Term.to_type ~env:tp_env tm in
+      let vl = Term.beta_reduce ?deep ~env:vl_env tm in
       Printf.printf "%s\n  : %s\n  = %s ;\n%!"
         (Id.to_string id)
         (Type.to_string tp)
-        (Term.to_string tm);
-      Id.Map.add id (Term.to_type tm) env
+        (Term.to_string vl);
+      Id.Map.add id tp tp_env, Id.Map.add id vl vl_env
     | Command.Eval_term tm ->
-      let tp = Term.to_type ~env tm in
-      let vl = Term.beta_reduce tm in
+      let tp = Term.to_type ~env:tp_env tm in
+      let vl = Term.beta_reduce ?deep ~env:vl_env tm in
       Printf.printf "%s\n  : %s\n  = %s ;\n%!"
         (Term.to_string tm)
         (Type.to_string tp)
         (Term.to_string vl);
-      env
+      (tp_env, vl_env)
   in
 
   try
     let cmds = Parser.commands Lexer.prog lexbuf in
-    List.fold_left evaluate_command env cmds
+    List.fold_left evaluate_command (tp_env, vl_env) cmds
   with
     | Parsing.Parse_error ->
       Printf.printf "Parser: error\n%!";
@@ -53,18 +62,20 @@ let main () =
 
   match !mode with
     | Repl ->
-      let rec eval_phrase (env : Type.t Id.Map.t) =
+      let rec eval_phrase tp_env vl_env =
         Printf.printf "> %!";
-        let env' =
-          evaluate env (Lexing.from_string @@ input_line stdin)
+        let tp_env', vl_env' =
+          evaluate tp_env vl_env @@
+            Lexing.from_string (input_line stdin)
         in
-        eval_phrase env'
+        eval_phrase tp_env' vl_env'
       in
-      eval_phrase Id.Map.empty
+      eval_phrase Id.Map.empty Id.Map.empty
     | File fs ->
       let eval_file f =
         let chan = open_in f in
-        ignore (evaluate Id.Map.empty @@ Lexing.from_channel chan);
+        ignore @@
+          evaluate Id.Map.empty Id.Map.empty (Lexing.from_channel chan);
         close_in chan;
       in
       List.iter eval_file @@ List.rev fs
