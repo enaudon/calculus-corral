aa3d50c45e2c64c70a1b72f8916f189d0bbeea23
Enrique Naudon <enaudoncs@gmail.com>
2018-07-07 09:46:56 -0400

    Changed tests to use the REPL's multi-file support

--- explicitly_typed/universal_types/exe/test/test_repl.ml
+++ explicitly_typed/universal_types/exe/test/test_repl.ml
@@ -2,25 +2,27 @@ open OUnit
 
 module Misc = Miscellaneous
 
-let assert_command repl file () =
+let assert_command repl dir file prelude () =
+  let files = List.map (Filename.concat dir) (prelude @ [file]) in
   let exit_code = Unix.WEXITED 0 in
   let foutput stream =
     let actual = Misc.char_stream_to_string stream in
-    let expect = Misc.file_to_string (file ^ ".exp") in
+    let expect =
+      String.concat "" @@
+        List.map (fun f -> Misc.file_to_string (f ^ ".exp")) files
+    in
     let printer str = str in
     assert_equal ~printer expect actual
   in
-  let args = ["--deep-beta-reduction"; file] in
+  let args = "--deep-beta-reduction" :: files in
   assert_command ~exit_code ~foutput repl args
 
 let make repl_exe case_dir =
-  let test_files =
-    let is_lc_file f =
-      Sys.file_exists f && not (Sys.is_directory f) &&
-        Filename.extension f = ".lc"
-    in
-    let fs = Array.to_list (Sys.readdir case_dir) in
-    List.filter is_lc_file @@ List.map (Filename.concat case_dir) fs
-  in
   "Repl" >:::
-    List.map (fun f -> f >:: assert_command repl_exe f) test_files
+    List.map
+      ( fun (file, pre) ->
+        file >:: assert_command repl_exe case_dir file pre)
+      [ ("function.lc", []);
+        ("pair.lc", []);
+        ("boolean.lc", ["pair.lc"]);
+        ("natural.lc", ["pair.lc"]); ]
--- implicitly_typed/monomorphic_types/exe/test/test_repl.ml
+++ implicitly_typed/monomorphic_types/exe/test/test_repl.ml
@@ -2,37 +2,36 @@ open OUnit
 
 module Misc = Miscellaneous
 
-let assert_command repl type_inf_alg file () =
+let assert_command repl inf_alg dir file prelude () =
+  let files = List.map (Filename.concat dir) (prelude @ [file]) in
   let exit_code = Unix.WEXITED 0 in
   let foutput stream =
     let actual = Misc.char_stream_to_string stream in
-    let expect = Misc.file_to_string (file ^ ".exp") in
+    let expect =
+      String.concat "" @@
+        List.map (fun f -> Misc.file_to_string (f ^ ".exp")) files
+    in
     let printer str = str in
     assert_equal ~printer expect actual
   in
-  let args = [
-    "--deep-beta-reduction" ;
-    "--type-inference-algorithm=" ^ type_inf_alg ;
-    file ;
-  ] in
+  let args =
+    "--deep-beta-reduction" ::
+    ("--type-inference-algorithm=" ^ inf_alg) ::
+    files
+  in
   assert_command ~exit_code ~foutput repl args
 
 let make repl_exe case_dir =
-  let test_files =
-    let is_lc_file f =
-      Sys.file_exists f && not (Sys.is_directory f) &&
-        Filename.extension f = ".lc"
-    in
-    let fs = Array.to_list (Sys.readdir case_dir) in
-    List.filter is_lc_file @@ List.map (Filename.concat case_dir) fs
+  let make_test type_inf_alg (file, prelude) =
+    file >:: assert_command repl_exe type_inf_alg case_dir file prelude
   in
+  let tests = [
+    ("function.lc", []) ;
+    ("pair.lc", []) ;
+    ("boolean.lc", ["pair.lc"]) ;
+    ("natural.lc", ["pair.lc"]) ;
+  ] in
   "Repl" >::: [
-    "Hindley_milner" >:::
-      List.map
-        (fun f -> f >:: assert_command repl_exe "hm" f)
-        test_files ;
-    "Pottier_remy" >:::
-      List.map
-        (fun f -> f >:: assert_command repl_exe "pr" f)
-        test_files ;
+    "Hindley_milner" >::: List.map (make_test "hm") tests ;
+    "Pottier_remy" >::: List.map (make_test "pr") tests ;
   ]
